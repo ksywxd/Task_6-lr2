777725d368c7563f57238ad5a657c521256a5975
ksywxd <xenia5224@gmail.com>
2025-09-19 21:52:36 +0300

    final

--- Task_6.cpp
+++ Task_6.cpp
@@ -7,7 +7,7 @@ int main()
     std::cout << "Enter x and D" << std::endl;
     std::cin >> x >> D;
     b = x + D;
-    A = D * x / b;
+    A = D * (x / b);
     S = (pow(A, 2) + b * cos(x));
     std::cout << S;
     return 0;
