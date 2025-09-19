90ee7b12c927066ada5f2f996c186e19ee51113c
ksywxd <xenia5224@gmail.com>
2025-09-19 21:34:07 +0300

    Добавьте файлы проекта.

---
+++ Task_6.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+    double x, D, A, b, S;
+    std::cout << "Enter x and D" << std::endl;
+    std::cin >> x >> D;
+    b = x + D;
+    A = D * x / b;
+    S = (pow(A, 2) + b * cos(x));
+    std::cout << S;
+    return 0;
+}
