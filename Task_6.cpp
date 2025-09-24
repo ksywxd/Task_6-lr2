3d96d0e6588630bb587a938476533e8f6c255e1d
ksywxd <xenia5224@gmail.com>
2025-09-24 23:28:38 +0300

    pow

--- Task_6.cpp
+++ Task_6.cpp
@@ -8,7 +8,7 @@ int main()
     std::cin >> x >> n >> m >> K;
     A = fabs(n + m);
     D = tan(x);
-    Y = 1.29 + K / A + powl(D, 2);
+    Y = 1.29 + K / A + pow(D, 2);
     std::cout << Y;
     return 0;
 }
