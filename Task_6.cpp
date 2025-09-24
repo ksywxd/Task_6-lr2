954d63f489ded8ae013608efcf4ac7594f13ef47
ksywxd <xenia5224@gmail.com>
2025-09-24 23:40:09 +0300

    Update Task_6.cpp
    division by zero

--- Task_6.cpp
+++ Task_6.cpp
@@ -8,6 +8,11 @@ int main()
     std::cin >> x >> p >> K >> C >> D;
     B = exp(K);
     A = x + sin(p);
+    if (A == 0 || B == 0) {
+    std::cout << "division by zero" << std::endl;
+    return 1;
+}
+
     Y = 1 + pow(K, 2) / (2 * A * B) - B + D * C;
     std::cout << Y;
     return 0;
