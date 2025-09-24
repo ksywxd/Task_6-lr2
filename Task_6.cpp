c815aea33455862385e984c80decfa9a4a7ac9e6
ksywxd <xenia5224@gmail.com>
2025-09-24 23:33:10 +0300

    division by zero

--- Task_6.cpp
+++ Task_6.cpp
@@ -8,6 +8,11 @@ int main()
     std::cin >> x >> y >> z >> K >> D >> C;
     A = x - y;
     B = sqrt(z);
+    if (K - C * D == 0) {
+        std::cout << "division by zero" << std::endl;
+        return 1;
+    }
+
     T = cos(x) + pow(A, 2) / (K - C * D) - B;
     std::cout << T;
     return 0;
