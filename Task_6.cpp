6988c22edcd407037aac739eb5315068a8ba72da
ksywxd <xenia5224@gmail.com>
2025-09-24 23:44:35 +0300

    division by zero for b

--- Task_6.cpp
+++ Task_6.cpp
@@ -7,8 +7,17 @@ int main()
     std::cout << "Enter x, D: " << std::endl;
     std::cin >> x >> D;
     b = x + D;
+    if (b == 0) {
+        std::cout << "division by zero" << std::endl;
+        return 1;
+    }
     A = D*(x/b);
-    S = (A * A + b * cos(x)) / (pow(D, 3) + (A + D - b));
+    double del = pow(D, 3) + (A + D - b);
+    if (del == 0) {
+        std::cout << "division by zero" << std::endl;
+        return 1;
+    }
+    S = (A * A + b * cos(x)) / del;
     std::cout <<S;
     return 0;
 }
