6022aa70161212138044b340a20227fe37e4ed04
ksywxd <xenia5224@gmail.com>
2025-09-24 23:23:11 +0300

    v1

--- Task_6.cpp
+++ Task_6.cpp
@@ -3,12 +3,12 @@
 
 int main()
 {
-    double x, A, B, p, K, Y, C, D;
-    std::cout << "Enter x, p, K, C, D" << std::endl;
-    std::cin >> x >> p >> K >> C >> D;
-    B = exp(K);
-    A = x + sin(p);
-    Y = 1 + powl(K, 2) / (2 * A * B) - B + D * C;
-    std::cout << Y;
+    double x, b, D, A, S;
+    std::cout << "Enter x, D: " << std::endl;
+    std::cin >> x >> D;
+    b = x + D;
+    A = D*(x/b);
+    S = (A * A + b * cos(x)) / (pow(D, 3) + (A + D - b));
+    std::cout <<S;
     return 0;
 }
