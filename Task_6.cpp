bdbe23543ef3ca9f0b278602418dd204cbc5383f
ksywxd <xenia5224@gmail.com>
2025-09-24 23:47:16 +0300

    division by zero

--- Task_6.cpp
+++ Task_6.cpp
@@ -6,6 +6,10 @@ int main()
     double x, A, K, y, C, D, S;
     std::cout << "Enter x, p, K, C" << std::endl;
     std::cin >> x >> y >> K >> C;
+    if (C == 0  || K == 0) {
+        std::cout << "division by zero" << std::endl;
+        return 1;
+    }
     A = x + y;
     D = fabs(C - A);
     S = 10.1 + A / C + D / pow(K, 2);
